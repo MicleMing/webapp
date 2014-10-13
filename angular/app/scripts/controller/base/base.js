d2b4e7e8292235186cd504f382c02df5fc6db29e
lanmingming <fzulanming@gmail.com>
2014-10-13 18:02:55 +0800

    deleted

--- angular/app/scripts/controller/base/base.js
+++
@@ -1,9 +0,0 @@
-
-/**
- * Created by Administrator on 2014/10/11.
- */
-var baseController = angular.module('baseController',[]);
-
-baseController.controller('baseCtr1',['$scope',function($scope){
-
-}])
