fb0d68bbc78ff418f872f5fa160a26e5216929d8
MicleMing <fzulanming@gmail.com>
2015-01-14 11:58:31 +0800

    1.调整UI

--- webapp/public/javascripts/controller/base/baseCtr.js
+++ webapp/public/javascripts/controller/base/baseCtr.js
@@ -4,7 +4,7 @@
  */
 var baseController = angular.module('baseController',[]);
 
-baseController.controller('baseCtrl',function($scope,$modal,$location,AuthService,ipCookie){
+baseController.controller('baseCtrl',function($scope,$rootScope,$modal,$location,AuthService,ipCookie){
     var vm = $scope.vm = {
         keyword:''
     };
@@ -44,6 +44,13 @@ baseController.controller('baseCtrl',function($scope,$modal,$location,AuthServic
      * @description 转跳到在线聊天
      */
     vm.gotoChat = function(){
-        $location.path('/chat');
+        if($rootScope.access_token){
+            $location.path('/chat');
+        }else{
+            $scope.$emit('error',{
+                title:'',
+                message:'请先登录！'
+            });
+        }
     }
 })
